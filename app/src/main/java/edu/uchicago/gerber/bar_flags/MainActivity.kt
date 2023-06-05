4f95973a6ad5ff25f3d7944e3af58fc8a8553540
Adam Gerber <adam.gerber@yahoo.com>
2023-06-05 18:40:14 -0500

    thai flag created

--- app/src/main/java/edu/uchicago/gerber/bar_flags/MainActivity.kt
+++ app/src/main/java/edu/uchicago/gerber/bar_flags/MainActivity.kt
@@ -4,11 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import edu.uchicago.gerber.bar_flags.ui.theme.t_blue
+import edu.uchicago.gerber.bar_flags.ui.theme.t_red
+import edu.uchicago.gerber.bar_flags.ui.theme.white
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,11 +33,37 @@ fun MainWrapper() {
     //Box - z axis
     // Row - x axis
     // Column - y axis
-    Box() {
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "UChicago")
-            Text(text = "MPCS")
-        }
+    Column() {
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            color = t_red
+        ) {}
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            color = white
+        ) {}
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(2f),
+            color = t_blue
+        ) {}
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            color = white
+        ) {}
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            color = t_red
+        ) {}
 
     }
 
--- app/src/main/java/edu/uchicago/gerber/bar_flags/ui/theme/Color.kt
+++ app/src/main/java/edu/uchicago/gerber/bar_flags/ui/theme/Color.kt
@@ -9,3 +9,10 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val t_red = Color(0xFFEF3340)
+val t_blue = Color(0xFF00247D)
+
+
+val white = Color(0xFFFFFFFF)
+val black = Color(0xFF000000)
