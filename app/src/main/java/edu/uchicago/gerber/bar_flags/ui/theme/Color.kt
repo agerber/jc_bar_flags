5e43983d1134a750646bf75de00382ae2f937ea9
Adam Gerber <adam.gerber@yahoo.com>
2023-06-05 20:47:20 -0500

    generate buttons
    (cherry picked from commit 809b0e9bc40d954f49642895a98cbf28e4460ce9)

--- app/src/main/java/edu/uchicago/gerber/bar_flags/ui/theme/Color.kt
+++ app/src/main/java/edu/uchicago/gerber/bar_flags/ui/theme/Color.kt
@@ -10,9 +10,24 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
+//shared
+val white = Color(0xFFFFFFFF)
+val black = Color(0xFF000000)
+
+//thai
 val t_red = Color(0xFFEF3340)
 val t_blue = Color(0xFF00247D)
 
+//estonia
+val e_blue = Color(0xFF0072CE)
 
-val white = Color(0xFFFFFFFF)
-val black = Color(0xFF000000)
+//indonesia
+val i_red = Color(0xFFff0000)
+
+//netherlands
+val n_red = Color(0xFFC8102E)
+val n_blue = Color(0xFF003DA5)
+
+//ukriane
+val u_blue = Color(0xFF0057B7)
+val u_yellow = Color(0xFFFFDD00)
